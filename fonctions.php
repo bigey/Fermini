b7b05b8821597885b076c75d80ec66c68ebf20b6
Frédéric Bigey <fr.bigey@gmail.com>
2015-05-20 11:58:28 +0200

    Change indentation

--- fonctions.php
+++ fonctions.php
@@ -11,17 +11,17 @@ $haut=<<<EOF
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<title>$texte</title>
-	<link rel="stylesheet" type="text/css" href="../view.css" media="all">
-	<script type="text/javascript" src="view.js"></script>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>$texte</title>
+<link rel="stylesheet" type="text/css" href="../view.css" media="all">
+<script type="text/javascript" src="view.js"></script>
 </head>
 <body id="main_body" >
-	<img id="top" src="top.png" alt="">
-	<div id="form_container">
+<img id="top" src="top.png" alt="">
+<div id="form_container">
 EOF;
 
-	echo $haut;
+echo $haut;
 }
 
 
@@ -29,14 +29,14 @@ EOF;
 function bas_de_page($text = "") {
 
 $bas=<<<BAS
-		<div id="footer">UMR 1083 Sciences Pour l'Oenologie - INRA</div>
-	</div>
-	<img id="bottom" src="bottom.png" alt="">
+<div id="footer">UMR 1083 Sciences Pour l'Oenologie - INRA</div>
+</div>
+<img id="bottom" src="bottom.png" alt="">
 </body>
 </html>
 BAS;
 
-	echo $bas;
+echo $bas;
 }
 
 
@@ -63,9 +63,7 @@ function balance($arg = "SP") {
 
 	$output = array();
  	$return_value = 0;
-
 	$commande = "/var/www/Fermini/Scripts/balance_ohaus.pl";
-
 	$string = exec("$commande $arg", $output, $return_value);
 
 	return( ($return_value==0) ? $string : NULL );
@@ -79,7 +77,6 @@ function export_data($filename,$arg) {
 	$commande=escapeshellcmd($commande);
 
 	system("cd /tmp && $commande", $return_value);
-
 	return($return_value);
 }
 
