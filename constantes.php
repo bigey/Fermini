6e7b2dbdfd7f7283324fba218b7e6391dac7bec8
Frédéric Bigey <fr.bigey@gmail.com>
2015-05-20 11:59:28 +0200

    Change indentation

--- constantes.php
+++ constantes.php
@@ -3,20 +3,16 @@
 /* Définition des constantes utilisables dans l'ensemble des scripts */
 
 // definition du PATH pour l'ensemble du site
-
-	define("SITE_PATH", '/var/www/Fermini');
+define("SITE_PATH", '/var/www/Fermini');
 
 // definitions des constantes de connection à la base
-
-	define("USER","www-data");
-	define("PASS", "");
-	define("SERVEUR", "localhost");
-	define("BASE", "Fermini");
+define("USER","www-data");
+define("PASS", "");
+define("SERVEUR", "localhost");
+define("BASE", "Fermini");
 
 // limite session
-
-	define("VALIDITE", 2700); // durée de validité de la session avec la fonction date("U"). date("U")+VALIDITE
-	define("PERIMEE", 604800); // durée pendant laquelle la session sera conservée dans le table "Session"
-
+define("VALIDITE", 2700); // durée de validité de la session avec la fonction date("U"). date("U")+VALIDITE
+define("PERIMEE", 604800); // durée pendant laquelle la session sera conservée dans le table "Session"
 
 ?>
